c9797419c1c8bbced292d35e8d5fdd5803e04cfc
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-08-08 07:41:19 -0300

    Add files via upload

---
+++ Site_2C_turmaB/Painel/conecta.php
@@ -0,0 +1,18 @@
+<?php
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "cadastro_cliente";
+
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+    if(mysqli_connect_errno()){
+        echo "ERRO DE CONEXÃO";
+    }
+/*     else{
+        echo "CONECTADO AO BANCO COM SUCESSO";
+    } */
+
+
+?>
