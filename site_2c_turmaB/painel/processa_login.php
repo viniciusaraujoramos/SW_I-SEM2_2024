d15180a0f4712ee8481a8e5d08e6f9b1eefddb1d
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-08-26 07:50:29 -0300

    arrumando

--- site_2c_turmaB/painel/processa_login.php
+++ site_2c_turmaB/painel/processa_login.php
@@ -1,23 +1,26 @@
 <?php
-    require('processa.php');
+    require('conecta.php');
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $consulta = "SELECT * FROM USUARIOS WHERE email = '$email' AND senha = '$senha'";
+    $consulta = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
 
     $resultado = $conexao->query($consulta);
     $registros = $resultado->num_rows;
     $resultado_usuario = mysqli_fetch_assoc($resultado);
-
-    //var_dump($resultado_usuario);
     
+    //var_dump($resultado_usuario);
 
     if($registros == 1){
-        //echo "TE ACHEI";
-        header('Location:index.php');
+        //echo "TE ACHEI";       
+        header('Location: index.php');
+
     }
     else{
-        //echo "NÃO ACHEI";
+        //echo "NÃO ACHEI";        
         header('Location: ../index.html');
     }
+
+
+
 ?>
